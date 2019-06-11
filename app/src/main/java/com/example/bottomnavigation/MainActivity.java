866d7fe6f9e4cc2fc48b846fa8762cc631e68356
uditsaurav <uditsourav21@gmail.com>
2019-06-12 01:29:12 +0530

    layout ready-1.0

--- app/src/main/java/com/example/bottomnavigation/MainActivity.java
+++ app/src/main/java/com/example/bottomnavigation/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.bottomnavigation;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
@@ -9,6 +12,9 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     ViewPager viewPager = null;
@@ -49,30 +55,61 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-
     }
 
     BottomNavigationView.OnNavigationItemSelectedListener navListner = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
             //Fragment fragment = null;
-            switch(menuItem.getItemId()){
-                case R.id.nav_schedule: viewPager.setCurrentItem(0);
-                                    break;
-                case R.id.nav_hostel: viewPager.setCurrentItem(1);
-                                    break;
-                case R.id.nav_home: viewPager.setCurrentItem(2);
-                                    break;
-                case R.id.nav_mess: viewPager.setCurrentItem(3);
-                                    break;
-                case R.id.nav_medical: viewPager.setCurrentItem(4);
-                                    break;
+            switch (menuItem.getItemId()) {
+                case R.id.nav_schedule:
+                    viewPager.setCurrentItem(0);
+                    break;
+                case R.id.nav_hostel:
+                    viewPager.setCurrentItem(1);
+                    break;
+                case R.id.nav_home:
+                    viewPager.setCurrentItem(2);
+                    break;
+                case R.id.nav_mess:
+                    viewPager.setCurrentItem(3);
+                    break;
+                case R.id.nav_medical:
+                    viewPager.setCurrentItem(4);
+                    break;
             }
             //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).commit();
             return true;
         }
     };
-    class MyAdapter extends FragmentStatePagerAdapter{
+
+    public void onclick_schedule(View v) {
+        Dialog student_dialog = new Dialog(MainActivity.this);
+        student_dialog.setContentView(R.layout.schedule_student_dialog);
+        TextView teacher_dialog, subject_dialog, room_dialog;
+        teacher_dialog = student_dialog.getWindow().findViewById(R.id.dialog_teacher);
+        subject_dialog = student_dialog.getWindow().findViewById(R.id.dialog_subject);
+        room_dialog = student_dialog.getWindow().findViewById(R.id.dialog_room);
+        if (v.getId() == R.id.b1) {
+            teacher_dialog.setText("B.K.Singh");
+            subject_dialog.setText("Operating Systems");
+            room_dialog.setText("204");
+        }
+        else if (v.getId() == R.id.b2) {
+            teacher_dialog.setText("R.R.Suman");
+            subject_dialog.setText("ACOA");
+            room_dialog.setText("202");
+        }
+        else if (v.getId() == R.id.b3) {
+            teacher_dialog.setText("Rabi Shaw");
+            subject_dialog.setText("DS");
+            room_dialog.setText("201");
+        }
+        student_dialog.setCanceledOnTouchOutside(true);
+        student_dialog.show();
+    }
+
+    class MyAdapter extends FragmentStatePagerAdapter {
 
         public MyAdapter(FragmentManager fm) {
             super(fm);
@@ -81,17 +118,22 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public Fragment getItem(int i) {
             Fragment fragment = null;
-            switch(i){
+            switch (i) {
 
-                case 0: fragment = new ScheduleFragment();
+                case 0:
+                    fragment = new ScheduleFragment();
                     break;
-                case 1: fragment = new HostelFragment();
+                case 1:
+                    fragment = new HostelFragment();
                     break;
-                case 2: fragment = new HomeFragment();
+                case 2:
+                    fragment = new HomeFragment();
                     break;
-                case 3: fragment = new MessFragment();
+                case 3:
+                    fragment = new MessFragment();
                     break;
-                case 4: fragment = new MedicalFragment();
+                case 4:
+                    fragment = new MedicalFragment();
                     break;
 
             }
--- app/src/main/java/com/example/bottomnavigation/ScheduleFragment.java
+++ app/src/main/java/com/example/bottomnavigation/ScheduleFragment.java
@@ -1,5 +1,8 @@
 package com.example.bottomnavigation;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,6 +11,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,39 +21,18 @@ import DataClass.ScheduleStudentListItemDataClass;
 
 public class ScheduleFragment extends Fragment {
 
-
-    RecyclerView recyclerView;
-    RecyclerView.Adapter adapter;
-    List<ScheduleStudentListItemDataClass> list;
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState){
-        View view = (View) inflater.inflate( R.layout.fragment_schedule, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
+        View view = (View) inflater.inflate(R.layout.fragment_schedule, container, false);
 
-        recyclerView=(RecyclerView)view.findViewById(R.id.fragment_schedule_recycler_view);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setClickable(false);
-
-        list=new ArrayList<>();
-
-        ScheduleStudentListItemDataClass x=new ScheduleStudentListItemDataClass("Operating System","BK SINGH",1,false);
-        list.add(x);
-
-        x=new ScheduleStudentListItemDataClass("Operating System","BK SINGH",2,true);
-        list.add(x);
-
-
-
-        adapter=new ScheduleStudentRecyclerViewAdapter(list,getContext());
-
-        recyclerView.setAdapter(adapter);
-
+        /*Dialog fbDialogue = new Dialog(getActivity(), android.R.style.Theme_Black_NoTitleBar);
+        fbDialogue.getWindow().setBackgroundDrawable(new ColorDrawable(Color.argb(100, 0, 0, 0)));
+        fbDialogue.setContentView(R.layout.schedule_student_dialog);
+        fbDialogue.setCancelable(true);
+        fbDialogue.show();*/
         return view;
 
     }
-
-
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
