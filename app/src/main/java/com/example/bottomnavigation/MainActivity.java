617ed51265239b82948495afb95af9820a27af87
uditsaurav <uditsourav21@gmail.com>
2019-06-12 16:53:18 +0530

    layout ready+somewhat implemented code

--- app/src/main/java/com/example/bottomnavigation/MainActivity.java
+++ app/src/main/java/com/example/bottomnavigation/MainActivity.java
@@ -16,6 +16,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import org.w3c.dom.Node;
+
 public class MainActivity extends AppCompatActivity {
     ViewPager viewPager = null;
     MenuItem prevMenuItem = null;
@@ -31,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(navListner);
-        //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HostelFragment()).commit();
         prevMenuItem = bottomNavigationView.getMenu().getItem(2).setChecked(false);
         viewPager.setCurrentItem(2);
         viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
@@ -43,7 +44,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onPageSelected(int i) {
                 bottomNavigationView.getMenu().getItem(0).setChecked(false);
-                //Log.d("page", "onPageSelected: "+position);
                 bottomNavigationView.getMenu().getItem(i).setChecked(true);
                 prevMenuItem = bottomNavigationView.getMenu().getItem(i);
             }
@@ -60,7 +60,6 @@ public class MainActivity extends AppCompatActivity {
     BottomNavigationView.OnNavigationItemSelectedListener navListner = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-            //Fragment fragment = null;
             switch (menuItem.getItemId()) {
                 case R.id.nav_schedule:
                     viewPager.setCurrentItem(0);
@@ -78,35 +77,25 @@ public class MainActivity extends AppCompatActivity {
                     viewPager.setCurrentItem(4);
                     break;
             }
-            //getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).commit();
             return true;
         }
     };
 
+
+
+
+
     public void onclick_schedule(View v) {
-        Dialog student_dialog = new Dialog(MainActivity.this);
-        student_dialog.setContentView(R.layout.schedule_student_dialog);
-        TextView teacher_dialog, subject_dialog, room_dialog;
-        teacher_dialog = student_dialog.getWindow().findViewById(R.id.dialog_teacher);
-        subject_dialog = student_dialog.getWindow().findViewById(R.id.dialog_subject);
-        room_dialog = student_dialog.getWindow().findViewById(R.id.dialog_room);
-        if (v.getId() == R.id.b1) {
-            teacher_dialog.setText("B.K.Singh");
-            subject_dialog.setText("Operating Systems");
-            room_dialog.setText("204");
-        }
-        else if (v.getId() == R.id.b2) {
-            teacher_dialog.setText("R.R.Suman");
-            subject_dialog.setText("ACOA");
-            room_dialog.setText("202");
-        }
-        else if (v.getId() == R.id.b3) {
-            teacher_dialog.setText("Rabi Shaw");
-            subject_dialog.setText("DS");
-            room_dialog.setText("201");
-        }
-        student_dialog.setCanceledOnTouchOutside(true);
-        student_dialog.show();
+//        Dialog student_dialog = new Dialog(MainActivity.this);
+//        student_dialog.setContentView(R.layout.schedule_student_dialog);
+//        TextView teacher_dialog, subject_dialog, room_dialog;
+//
+//        teacher_dialog = student_dialog.getWindow().findViewById(R.id.dialog_teacher);
+//        subject_dialog = student_dialog.getWindow().findViewById(R.id.dialog_subject);
+//        room_dialog = student_dialog.getWindow().findViewById(R.id.dialog_room);
+//
+//        student_dialog.setCanceledOnTouchOutside(true);
+//        student_dialog.show();
     }
 
     class MyAdapter extends FragmentStatePagerAdapter {
@@ -119,7 +108,6 @@ public class MainActivity extends AppCompatActivity {
         public Fragment getItem(int i) {
             Fragment fragment = null;
             switch (i) {
-
                 case 0:
                     fragment = new ScheduleFragment();
                     break;
@@ -135,7 +123,6 @@ public class MainActivity extends AppCompatActivity {
                 case 4:
                     fragment = new MedicalFragment();
                     break;
-
             }
 
             return fragment;
@@ -146,4 +133,6 @@ public class MainActivity extends AppCompatActivity {
             return 5;
         }
     }
+
+
 }
--- app/src/main/java/com/example/bottomnavigation/ScheduleFragment.java
+++ app/src/main/java/com/example/bottomnavigation/ScheduleFragment.java
@@ -12,6 +12,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Node;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,17 +26,75 @@ import DataClass.ScheduleStudentListItemDataClass;
 
 public class ScheduleFragment extends Fragment {
 
+    class Node {
+        String teacher_name, subject_name, subject_code, room_no;
+
+        public Node() {
+            this.teacher_name = "Teacher Name";
+            this.subject_name = "subject_name";
+            this.subject_code = "subject_code";
+            this.room_no = "room_no";
+        }
+    }
+
+    Node[] node;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
         View view = (View) inflater.inflate(R.layout.fragment_schedule, container, false);
+        generateObjectArrayNode(view);
 
-        /*Dialog fbDialogue = new Dialog(getActivity(), android.R.style.Theme_Black_NoTitleBar);
-        fbDialogue.getWindow().setBackgroundDrawable(new ColorDrawable(Color.argb(100, 0, 0, 0)));
-        fbDialogue.setContentView(R.layout.schedule_student_dialog);
-        fbDialogue.setCancelable(true);
-        fbDialogue.show();*/
         return view;
+    }
 
+    public void generateObjectArrayNode(View view) {
+        node = new Node[41];
+        for (int i = 0; i < 41; i++)
+            node[i] = new Node();
+        setButtons(view);
+    }
+
+    private void setButtons(View view) {
+        char x = 'b', y = '1';
+        for (int i = 1; i <= 40; i++) {
+            String sId = Character.toString(x) + "" + Character.toString(y);
+            int id = getResources().getIdentifier(sId, "id", getActivity().getPackageName());
+            if (id == 0) {
+                Toast.makeText(getContext(), sId + "NULL", Toast.LENGTH_SHORT).show();
+            } else {
+                node[i].teacher_name = "RABI SHAW";
+                node[i].subject_name = "OPERATING SYSTEMS";
+                node[i].room_no = "204";
+                final int j=i;
+
+                Button button = view.findViewById(id);
+                button.setText("CS501");
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Dialog student_dialog = new Dialog(getContext());
+                        student_dialog.setContentView(R.layout.schedule_student_dialog);
+
+                        TextView teacher_dialog, subject_dialog, room_dialog;
+                        teacher_dialog = student_dialog.getWindow().findViewById(R.id.dialog_teacher);
+                        subject_dialog = student_dialog.getWindow().findViewById(R.id.dialog_subject);
+                        room_dialog = student_dialog.getWindow().findViewById(R.id.dialog_room);
+                        teacher_dialog.setText(node[j].teacher_name);
+                        subject_dialog.setText(node[j].subject_name);
+                        room_dialog.setText(node[j].room_no);
+
+                        student_dialog.setCanceledOnTouchOutside(true);
+                        student_dialog.show();
+                    }
+                });
+            }
+            y++;
+
+            if (y > '5') {
+                y = '1';
+                x++;
+            }
+        }
     }
 
     @Override
