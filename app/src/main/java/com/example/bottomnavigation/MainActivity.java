3f456ab9e30faf821ff95c37fab8828e0e8671c6
uditsaurav <uditsourav21@gmail.com>
2019-06-18 17:37:04 +0530

    schedule layout + sharedPref(faculty)

--- app/src/main/java/com/example/bottomnavigation/MainActivity.java
+++ app/src/main/java/com/example/bottomnavigation/MainActivity.java
@@ -1,9 +1,6 @@
 package com.example.bottomnavigation;
 
-import android.app.Dialog;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
@@ -13,11 +10,6 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import org.w3c.dom.Node;
 
 public class MainActivity extends AppCompatActivity {
     ViewPager viewPager = null;
@@ -94,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
 
     class MyAdapter extends FragmentStatePagerAdapter {
 
-        public MyAdapter(FragmentManager fm) {
+        MyAdapter(FragmentManager fm) {
             super(fm);
         }
 
--- app/src/main/java/com/example/bottomnavigation/ScheduleFragment.java
+++ app/src/main/java/com/example/bottomnavigation/ScheduleFragment.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,12 +26,12 @@ import DataClass.ScheduleStudentListItemDataClass;
 
 public class ScheduleFragment extends Fragment {
 
-    SharedPreferences sharedPreferences;
     public String MYPREFERENCE = "mypref";
     public String SubcodeKey = "subcode";
     public String SubnameKey = "subname";
     public String TeachernameKey = "teachername";
     public String RoomnoKey = "roomno";
+    boolean access = true;
 
     class Node {
         String teacher_name, subject_name, subject_code, room_no;
@@ -49,7 +50,6 @@ public class ScheduleFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
         View view = inflater.inflate(R.layout.fragment_schedule, container, false);
         generateObjectArrayNode(view);
-        sharedPreferences = getContext().getSharedPreferences(MYPREFERENCE, Context.MODE_PRIVATE);
 
         return view;
     }
@@ -80,31 +80,78 @@ public class ScheduleFragment extends Fragment {
                 ViewGroup.LayoutParams lp = button.getLayoutParams();
                 ((LinearLayout.LayoutParams) lp).setMargins(2, 0, 3, 0);
                 button.setOnClickListener(new View.OnClickListener() {
+
+                    int flag = 0;
+                    EditText subject_code_et, subject_name_et, teacher_name_et, room_no_et;
+                    Dialog teacher_dialog = new Dialog(getContext());
+                    TextView subject_code_dialog, subject_dialog, room_dialog;
+                    Dialog student_dialog = new Dialog(getContext());
+
                     @Override
                     public void onClick(View v) {
-                        Dialog student_dialog = new Dialog(getContext());
-                        student_dialog.setContentView(R.layout.schedule_student_dialog);
+                        if (access == true) {
+                            teacher_dialog.setContentView(R.layout.schedule_teacher_dialog);
 
-                        TextView subject_code_dialog, subject_dialog, room_dialog;
-                        Button teacher_dialog;
-                        teacher_dialog = student_dialog.getWindow().findViewById(R.id.dialog_teacher);
-                        subject_code_dialog = student_dialog.getWindow().findViewById(R.id.dialog_subject_code);
-                        subject_dialog = student_dialog.getWindow().findViewById(R.id.dialog_subject);
-                        room_dialog = student_dialog.getWindow().findViewById(R.id.dialog_room);
-                        subject_code_dialog.setText(node[j].subject_code);
-                        subject_dialog.setText(node[j].subject_name);
-                        teacher_dialog.setText(node[j].teacher_name);
-                        room_dialog.setText(node[j].room_no);
+                            subject_code_et = teacher_dialog.getWindow().findViewById(R.id.edit_dialog_sub_code);
+                            subject_name_et = teacher_dialog.getWindow().findViewById(R.id.edit_dialog_sub_name);
+                            teacher_name_et = teacher_dialog.getWindow().findViewById(R.id.edit_dialog_teacher_name);
+                            room_no_et = teacher_dialog.getWindow().findViewById(R.id.edit_dialog_room_no);
 
-                        SharedPreferences.Editor editor = sharedPreferences.edit();
-                        editor.putString(SubcodeKey + sId, node[j].subject_code);
-                        editor.putString(SubnameKey + sId, node[j].subject_name);
-                        editor.putString(TeachernameKey + sId, node[j].teacher_name);
-                        editor.putString(RoomnoKey + sId, node[j].room_no);
-                        editor.apply();
+                            if (flag == 1) {
+                                SharedPreferences sp = getContext().getSharedPreferences(MYPREFERENCE, Context.MODE_PRIVATE);
+                                String subject_code_sp = sp.getString(SubcodeKey + sId, "");
+                                String subject_name_sp = sp.getString(SubnameKey + sId, "");
+                                String teacher_name_sp = sp.getString(TeachernameKey + sId, "");
+                                String room_no_sp = sp.getString(RoomnoKey + sId, "");
+                                subject_code_et.setText(subject_code_sp);
+                                subject_name_et.setText(subject_name_sp);
+                                teacher_name_et.setText(teacher_name_sp);
+                                room_no_et.setText(room_no_sp);
+                            }
+                            Button save_schedule_btn = teacher_dialog.getWindow().findViewById(R.id.button_save_schedule);
+                            save_schedule_btn.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    SharedPreferences sharedPref = getContext().getSharedPreferences(MYPREFERENCE, Context.MODE_PRIVATE);
+                                    SharedPreferences.Editor editor = sharedPref.edit();
+                                    editor.putString(SubcodeKey + sId, subject_code_et.getText().toString());
+                                    editor.putString(SubnameKey + sId, subject_name_et.getText().toString());
+                                    editor.putString(TeachernameKey + sId, teacher_name_et.getText().toString());
+                                    editor.putString(RoomnoKey + sId, room_no_et.getText().toString());
+                                    editor.apply();
+                                    flag = 1;
+                                    Toast.makeText(getContext(), "saved", Toast.LENGTH_SHORT).show();
+                                    teacher_dialog.dismiss();
+                                }
+                            });
 
-                        student_dialog.setCanceledOnTouchOutside(true);
-                        student_dialog.show();
+                            teacher_dialog.setCanceledOnTouchOutside(true);
+                            teacher_dialog.show();
+                        } else {
+
+                            student_dialog.setContentView(R.layout.schedule_student_dialog);
+
+                            Button teacher_info;
+                            teacher_info = student_dialog.getWindow().findViewById(R.id.dialog_teacher);
+                            subject_code_dialog = student_dialog.getWindow().findViewById(R.id.dialog_subject_code);
+                            subject_dialog = student_dialog.getWindow().findViewById(R.id.dialog_subject);
+                            room_dialog = student_dialog.getWindow().findViewById(R.id.dialog_room);
+                            subject_code_dialog.setText(node[j].subject_code);
+                            subject_dialog.setText(node[j].subject_name);
+                            teacher_info.setText(node[j].teacher_name);
+                            room_dialog.setText(node[j].room_no);
+
+                            SharedPreferences sp1 = getContext().getSharedPreferences(MYPREFERENCE, Context.MODE_PRIVATE);
+                            SharedPreferences.Editor editor = sp1.edit();
+                            editor.putString(SubcodeKey + sId, node[j].subject_code);
+                            editor.putString(SubnameKey + sId, node[j].subject_name);
+                            editor.putString(TeachernameKey + sId, node[j].teacher_name);
+                            editor.putString(RoomnoKey + sId, node[j].room_no);
+                            editor.apply();
+
+                            student_dialog.setCanceledOnTouchOutside(true);
+                            student_dialog.show();
+                        }
                     }
                 });
             }
